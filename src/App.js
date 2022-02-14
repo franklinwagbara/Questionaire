9ee8e9b599da68d4944559c009c8df75dd33e307
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-02-14 14:15:51 +0100

    first deployment

--- src/App.js
+++ src/App.js
@@ -4,18 +4,46 @@ import Questionaire from "./components/questionaire";
 import { getQuestions } from "./questions";
 
 class App extends Component {
-  state = { questions: [], number: 1 };
+  state = { questions: [], number: 0, score: 0 };
 
   componentDidMount = () => {
     const questions = getQuestions();
     this.setState({ questions });
     document.body.style.backgroundColor = "#DBD5E7";
   };
+
+  handleSubmit = (correct) => {
+    this.setState({
+      number: this.state.number + 1,
+      score: this.state.score + correct,
+    });
+  };
+
   render() {
     const { questions, number } = this.state;
+
+    if (
+      this.state.number &&
+      this.state.number === this.state.questions.length
+    ) {
+      //alert("You have finished the questionaire!");
+      return (
+        <div className="container">
+          <h1 className="text-danger">You have finished the questionaire!</h1>
+          <h2>Your Score is</h2>
+          <h4>
+            {this.state.score}/{this.state.questions.length}
+          </h4>
+        </div>
+      );
+    }
     return (
       <div className="container">
-        <Questionaire question={questions[number]} number={number} />
+        <Questionaire
+          question={questions[number]}
+          number={number}
+          onSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
--- src/components/questionaire.jsx
+++ src/components/questionaire.jsx
@@ -2,17 +2,85 @@ import React, { Component } from "react";
 import "./css/questionaire.css";
 
 class Questionaire extends Component {
-  state = {};
-  render() {
-    const { question, number } = this.props;
+  state = { correct: 0, selected: null };
 
-    if (!question) return <div>We could show any questions!</div>;
+  handleChange = (e) => {
+    const correct = e.target.value === this.props.question.answer ? 1 : 0;
+    this.setState({ correct: correct, selected: e.target.value });
+  };
+
+  handleSubmit = () => {
+    if (!this.state.selected) {
+      alert("You must select and option!");
+      return;
+    }
+    this.setState({ selected: null });
+    this.props.onSubmit(this.state.correct);
+  };
+
+  render() {
+    const { question, number, onSubmit } = this.props;
+
+    if (!question) return <div>We could not show any questions!</div>;
+
     return (
       <div className="questionaire">
         <h3>{question.question}</h3>
-        <input type="radio" id={number} name="number" value="HTML" />
-        <label htmlFor="html">{question.a}</label>
-        <br />
+        <ul>
+          <li>
+            <input
+              type="radio"
+              id="a"
+              name="answer"
+              value={"a"}
+              checked={this.state.selected === "a"}
+              onChange={this.handleChange}
+            />
+            <label id="a" htmlFor="a">
+              {question.a}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              id="b"
+              name="answer"
+              value={"b"}
+              checked={this.state.selected === "b"}
+              onChange={this.handleChange}
+            />
+            <label id="b" htmlFor="b">
+              {question.b}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              id="c"
+              name="answer"
+              value={"c"}
+              checked={this.state.selected === "c"}
+              onChange={this.handleChange}
+            />
+            <label id="c" htmlFor="c">
+              {question.c}
+            </label>
+          </li>
+          <li>
+            <input
+              type="radio"
+              id="d"
+              name="answer"
+              value={"d"}
+              checked={this.state.selected === "d"}
+              onChange={this.handleChange}
+            />
+            <label id="d" htmlFor="d">
+              {question.d}
+            </label>
+          </li>
+        </ul>
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
